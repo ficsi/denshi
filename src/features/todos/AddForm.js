4cabfb6f542504e6d163817d791279aa31cd2f3c
dimata <dimitar.serafimov@egt-bg.com>
2024-08-30 10:19:43 +0300

    fix clear input

--- src/features/todos/AddForm.js
+++ src/features/todos/AddForm.js
@@ -5,22 +5,23 @@ import {addTodo} from "./todosSlice";
 
 function AddForm() {
 	const dispatch = useDispatch();
-	const [todo, setTodo] = useState({});
+	const [todo, setTodo] = useState('');
 
 	const handleTitleChange = (e) => {
 		setTodo(e.target.value);
 	}
 	const handleAddTodo = (e) => {
 		e.preventDefault();
-		if (todo.trim()) { // Check if todo is not just whitespace
-			dispatch(addTodo({ id: nanoid(), title: todo, completed: false }));
+		console.log(todo)
+		if (todo) {
+			dispatch(addTodo({id: nanoid(), title: todo, completed: false}));
 			setTodo('');
 		}
 	};
 
 	return (
 		<form action="" className={'add-todo-form'}>
-			<input onChange={(e) => handleTitleChange(e)} value={todo.title} type="text"/>
+			<input onChange={(e) => handleTitleChange(e)} value={todo} type="text"/>
 			<button onClick={(e) => handleAddTodo(e)}>Add Task</button>
 		</form>
 	)
